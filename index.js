adc47dcaec5907d5298b95dce79a64bf8e550c16
Brandon Palomino <12bpalomino@gmail.com>
2018-10-27 18:02:33 -0700

    added page verify token

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.access_token
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -36,7 +38,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  let VERIFY_TOKEN = "honeyToken"
     
   // Parse the query params
   let mode = req.query['hub.mode'];
